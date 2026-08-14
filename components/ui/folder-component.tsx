39159837b07419c67a904d8d70c54bbffc16849d
Mik <mik23@protonmail.ch>
2026-08-14 16:37:30 +0530

    refactor(folder-component): extract papers animation and optimize flap blur

--- components/ui/folder-component.tsx
+++ components/ui/folder-component.tsx
@@ -172,6 +172,67 @@ const BASE_HEIGHT = 270;
 const FLAP_PATH =
   "M0 25C0 11.1929 11.1929 0 25 0H136.084C143.044 0 149.689 2.90139 154.42 8.00608L178.08 33.5343C182.811 38.639 189.456 41.5404 196.416 41.5404H296C309.807 41.5404 321 52.7333 321 66.5404V216C321 229.807 309.807 241 296 241H25C11.1929 241 0 229.807 0 216V25Z";
 
+const FLAP_WIDTH = 321;
+const FLAP_HEIGHT = 241;
+const CARD_SPRING = { type: "spring" as const, stiffness: 120, damping: 13 };
+
+const Papers = ({
+  uid,
+  theme,
+  isHovered,
+  isOpen,
+}: {
+  uid: string;
+  theme: Theme;
+  isHovered: boolean;
+  isOpen: boolean;
+}) => (
+  <>
+    <motion.div
+      className="absolute"
+      animate={{
+        y: isOpen ? -160 : isHovered ? -30 : -10,
+        x: isOpen ? 70 : 40,
+        rotate: isOpen ? 18 : isHovered ? 14 : 10,
+      }}
+      transition={{
+        ...CARD_SPRING,
+        delay: isOpen ? 0.1 : isHovered ? 0.12 : 0,
+      }}
+    >
+      <Card id={1} uid={uid} theme={theme} />
+    </motion.div>
+    <motion.div
+      className="absolute"
+      animate={{
+        y: isOpen ? -180 : isHovered ? -35 : -20,
+        x: isOpen ? 0 : 3,
+        rotate: isOpen ? -3 : isHovered ? -1 : 2,
+      }}
+      transition={{
+        ...CARD_SPRING,
+        delay: isOpen ? 0.05 : isHovered ? 0.06 : 0,
+      }}
+    >
+      <Card id={2} uid={uid} theme={theme} />
+    </motion.div>
+    <motion.div
+      className="absolute"
+      animate={{
+        y: isOpen ? -170 : isHovered ? -44 : -22,
+        x: isOpen ? -65 : -40,
+        rotate: isOpen ? -14 : isHovered ? -9 : -5,
+      }}
+      transition={{
+        ...CARD_SPRING,
+        delay: isOpen ? 0 : 0,
+      }}
+    >
+      <Card id={3} uid={uid} theme={theme} />
+    </motion.div>
+  </>
+);
+
 const FolderComponent = ({
   color = "black",
   size = "md",
@@ -184,6 +245,7 @@ const FolderComponent = ({
   const [isHovered, setIsHovered] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
   const flapFilterId = `folder-flap-${uid}`;
+  const flapBlurId = `folder-flap-blur-${uid}`;
 
   return (
     <div
@@ -231,84 +293,38 @@ const FolderComponent = ({
           </div>
 
           <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center">
-            <motion.div
-              className="absolute"
-              animate={{
-                y: isOpen ? -160 : isHovered ? -30 : -10,
-                x: isOpen ? 70 : 40,
-                rotate: isOpen ? 18 : isHovered ? 14 : 10,
-              }}
-              transition={{
-                type: "spring",
-                stiffness: 120,
-                damping: 13,
-                delay: isOpen ? 0.1 : isHovered ? 0.12 : 0,
-              }}
-            >
-              <Card id={1} uid={uid} theme={theme} />
-            </motion.div>
-            <motion.div
-              className="absolute"
-              animate={{
-                y: isOpen ? -180 : isHovered ? -35 : -20,
-                x: isOpen ? 0 : 3,
-                rotate: isOpen ? -3 : isHovered ? -1 : 2,
-              }}
-              transition={{
-                type: "spring",
-                stiffness: 120,
-                damping: 13,
-                delay: isOpen ? 0.05 : isHovered ? 0.06 : 0,
-              }}
-            >
-              <Card id={2} uid={uid} theme={theme} />
-            </motion.div>
-            <motion.div
-              className="absolute"
-              animate={{
-                y: isOpen ? -170 : isHovered ? -44 : -22,
-                x: isOpen ? -65 : -40,
-                rotate: isOpen ? -14 : isHovered ? -9 : -5,
-              }}
-              transition={{
-                type: "spring",
-                stiffness: 120,
-                damping: 13,
-                delay: isOpen ? 0 : 0,
-              }}
-            >
-              <Card id={3} uid={uid} theme={theme} />
-            </motion.div>
+            <Papers
+              uid={uid}
+              theme={theme}
+              isHovered={isHovered}
+              isOpen={isOpen}
+            />
           </div>
 
           <motion.div
             className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 mt-4"
             style={{
               transformOrigin: "bottom center",
-              transformStyle: "preserve-3d",
-              width: 321,
-              height: 241,
+              width: FLAP_WIDTH,
+              height: FLAP_HEIGHT,
             }}
             animate={{ rotateX: isOpen ? -55 : isHovered ? -45 : -15 }}
             transition={{ type: "spring", stiffness: 120, damping: 14 }}
           >
             <div
-              className="absolute inset-0"
+              className="pointer-events-none absolute inset-0"
               style={{
-                backdropFilter: "blur(6px)",
-                WebkitBackdropFilter: "blur(6px)",
                 clipPath: `path('${FLAP_PATH}')`,
                 WebkitClipPath: `path('${FLAP_PATH}')`,
-                transform: "translateZ(0)",
-                backfaceVisibility: "hidden",
-                WebkitBackfaceVisibility: "hidden",
-                willChange: "transform",
+                backdropFilter: "blur(6px)",
+                WebkitBackdropFilter: "blur(6px)",
+                backgroundColor: "rgb(255 255 255 / 0.01)",
               }}
             />
             <svg
               className="absolute inset-0"
-              width="321"
-              height="241"
+              width={FLAP_WIDTH}
+              height={FLAP_HEIGHT}
               viewBox="0 0 321 241"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
@@ -318,6 +334,7 @@ const FolderComponent = ({
                   d={FLAP_PATH}
                   fill={theme.flapFill}
                   fillOpacity={theme.flapFillOpacity}
+                  filter={`url(#${flapBlurId})`}
                 />
                 <path
                   d="M25 0.5H136.084C142.905 0.5 149.417 3.3431 154.054 8.3457L177.713 33.874C182.539 39.0808 189.317 42.04 196.416 42.04H296C309.531 42.04 320.5 53.0092 320.5 66.54V216C320.5 229.531 309.531 240.5 296 240.5H25C11.469 240.5 0.5 229.531 0.5 216V25C0.5 11.469 11.469 0.5 25 0.5Z"
@@ -325,6 +342,15 @@ const FolderComponent = ({
                 />
               </g>
               <defs>
+                <filter
+                  id={flapBlurId}
+                  x="-20%"
+                  y="-20%"
+                  width="140%"
+                  height="140%"
+                >
+                  <feGaussianBlur stdDeviation="3" />
+                </filter>
                 <filter
                   id={flapFilterId}
                   x="-25.4"
